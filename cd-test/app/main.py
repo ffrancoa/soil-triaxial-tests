670813934eec0892e70507f380b00cc09fe27441
ffrancoa <ffrancoa@uni.pe>
2022-03-26 18:35:56 -0500

    Tengo hambre

--- cd-test/app/main.py
+++ cd-test/app/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import plotly.graph_objects as go
+from plotly import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 
 st.set_page_config(
