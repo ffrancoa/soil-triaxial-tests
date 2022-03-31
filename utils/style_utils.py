2dd900e6e664cead211198ccf745603cda90c5e3
ffrancoa <ffrancoa@uni.pe>
2022-03-31 13:10:51 -0500

    n try.

--- utils/style_utils.py
+++ utils/style_utils.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 
-CUSTOM_FONT = "Roboto Mono"
+CUSTOM_FONT = "JetBrains Mono"
 
 CUSTOM_FONT_URL = """<link rel="preconnect" href="https://fonts.googleapis.com">
                      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">"""
+                     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">"""
 
 THEMES = {
     "nord-light": {
