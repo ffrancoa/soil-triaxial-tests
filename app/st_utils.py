3039eb5695e492e77250ade9114c5c7cf0c353b0
ffrancoa <ffrancoa@uni.pe>
2022-03-31 20:58:07 -0500

    Block type on google fonts import.

--- app/st_utils.py
+++ app/st_utils.py
@@ -7,7 +7,7 @@ CUSTOM_FONT = "JetBrains Mono"
 
 CUSTOM_FONT_URL = """<link rel="preconnect" href="https://fonts.googleapis.com">
                      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet">"""
+                     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=block" rel="stylesheet">"""
 
 THEMES = {
     "nord-light": {
